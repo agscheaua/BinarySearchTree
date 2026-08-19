cca5459ef2f0f53bb378c6b02a84dc64ea4a4850
Alex <agscheaua@gmail.com>
2026-08-19 23:03:02 +0300

    i need to inplement a sorting algorithm to sort the array before we create a BST form it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,101 @@
 import './styles.css'
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.leftNode;
+    this.rightNode;
+  };
+};
+
+class Tree {
+  constructor(){
+    this.root;
+  };
+
+  #buildTree(arr) {
+    console.log(arr);
+  };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const arr = [1,3,6,2,5,4];
+const mySet = new Set(arr);
+let arr2 = Array.from(mySet.values());
+console.log(arr2);
+
+let resultArray = [];
+const mergeSort = function(arr) {
+  if (!(Array.isArray(arr))) throw new Error("The argument is not an array.");
+  if (arr.length === 0) return [];
+  if (arr.length === 1) return arr;
+  if (arr.length > 1) {
+    let middleOfArray = Math.floor(arr.length / 2);
+
+    const leftHalf = arr.slice(0,middleOfArray);
+    const rightHalf = arr.slice(middleOfArray, arr.length);
+
+    const firstNum = mergeSort(leftHalf);
+    const secondNum = mergeSort(rightHalf);  
+
+    if (resultArray[0] === undefined) {
+      if (firstNum[0] < secondNum[0]) {
+        resultArray.push(firstNum[0]);
+        resultArray.push(secondNum[0]);
+      } else {
+        resultArray.push(secondNum[0]);
+        resultArray.push(firstNum[0]);
+      };
+      return resultArray;
+    };
+  
+    let sortingArray = [];
+    if (resultArray[0] !== undefined) {
+      let i = 0;
+      let j = 0;
+      for (;i < firstNum.length, j < secondNum.length;) {
+        if (firstNum[i] > secondNum[j]) {
+          sortingArray.push(secondNum[j]);
+          j++;
+        } else if (firstNum[i] < secondNum[j]) {
+          sortingArray.push(firstNum[i]);
+          i++;
+        } else if (firstNum[i] === secondNum[j]) {
+          sortingArray.push(firstNum[i]);
+          sortingArray.push(secondNum[j]);
+          i++;
+          j++;
+        };
+        
+        if (i === firstNum.length) {
+          for (let k = j; k < secondNum.length; k++) {
+            sortingArray.push(secondNum[k]);
+          };
+          resultArray = sortingArray;
+          return resultArray;
+        };
+        if (j === secondNum.length) {
+          for (let k = i; k < firstNum.length; k++) {
+            sortingArray.push(firstNum[k]);
+          };
+          resultArray = sortingArray;
+          return resultArray;
+        };
+      };
+    };
+  };
+};
+console.log(mergeSort(arr));
