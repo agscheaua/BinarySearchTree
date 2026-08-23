8e48b1e7be26f6782780ad953f33b2145afa2b2b
Alex <agscheaua@gmail.com>
2026-08-23 22:56:12 +0300

    i recreated a mergeSort algorithm to sort my array before i use its values to construct a BBST

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles.css'
 
+export {mergeSort};
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -19,24 +21,67 @@ class Tree {
 };
 
 
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-
-
-
-
-
-
-
-
-
-
-
-
-const arr = [1,3,6,2,5,4];
 const mySet = new Set(arr);
-let arr2 = Array.from(mySet.values());
-console.log(arr2);
+const arrWithoutDuplicates = Array.from(mySet.values());
+console.log(arrWithoutDuplicates);
 
+const mergeSort = function(arr) {
+  if (!(Array.isArray(arr))) throw new Error("The argument is not an array.");
+  if (arr.length === 0) return [];
+  if (arr.length === 1) return arr;
+  if (arr.length > 1) {
+    const middleOfArr = Math.floor(arr.length / 2);
+    let tempSortResultOfCurrIteration = [];
+
+    const leftHalfOfArr = arr.slice(0, middleOfArr);
+    const rightHalfOfArr = arr.slice(middleOfArr);
+
+    const mergeSortLeftHalfOfArr = mergeSort(leftHalfOfArr);
+    const mergeSortRightHalfOfArr = mergeSort(rightHalfOfArr);
+
+    for (let i = 0, j = 0; i < mergeSortLeftHalfOfArr.length &&
+         j < mergeSortRightHalfOfArr.length;) {
+      if (mergeSortLeftHalfOfArr[i] < mergeSortRightHalfOfArr[j]) {
+        tempSortResultOfCurrIteration.push(mergeSortLeftHalfOfArr[i]);
+        i++;
+      };
+      if (mergeSortLeftHalfOfArr[i] > mergeSortRightHalfOfArr[j]) {
+        tempSortResultOfCurrIteration.push(mergeSortRightHalfOfArr[j]);
+        j++;
+      };
+      if (i === mergeSortLeftHalfOfArr.length) {
+        for (;j < mergeSortRightHalfOfArr.length; j++) {
+          tempSortResultOfCurrIteration.push(mergeSortRightHalfOfArr[j])
+        };
+        break;
+      };
+      if (j === mergeSortRightHalfOfArr.length) {
+        for (;i < mergeSortLeftHalfOfArr.length; i++) {
+          tempSortResultOfCurrIteration.push(mergeSortLeftHalfOfArr[i]);
+        };
+        break;
+      };
+    };
+    return tempSortResultOfCurrIteration;
+  };
+
+};
+const sortedArr = mergeSort(arrWithoutDuplicates);
+console.log(sortedArr);
+
+
+
+
+
+
+
+
+
+
+
+/*
 let resultArray = [];
 const mergeSort = function(arr) {
   if (!(Array.isArray(arr))) throw new Error("The argument is not an array.");
@@ -99,3 +144,4 @@ const mergeSort = function(arr) {
   };
 };
 console.log(mergeSort(arr));
+*/
