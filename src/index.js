e1ffa5fb5d11450b6dfa0a4974e742f6bf313c84
Alex <agscheaua@gmail.com>
2026-08-24 23:02:04 +0300

    i created the algorithm to construct a BBST from a non-duplicated value, sorted array, further refac toring and testing is needed

--- src/index.js
+++ src/index.js
@@ -1,28 +1,87 @@
 import './styles.css'
 
-export {mergeSort};
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.leftNode;
-    this.rightNode;
+  constructor(rootNode = null, leftNode = null, rightNode = null) {
+    this.rootNode = rootNode;
+    this.leftNode = leftNode;
+    this.rightNode = rightNode;
   };
 };
 
 class Tree {
-  constructor(){
-    this.root;
+  constructor(arr) {
+    this.root = this.buildTree(Tree.sortTheArr(Tree.removeDuplicatesFromArr(arr)));
   };
 
-  #buildTree(arr) {
-    console.log(arr);
+  static removeDuplicatesFromArr(arr) {
+    const mySet = new Set(arr);
+    const arrWithoutDuplicates = Array.from(mySet.values());
+    return arrWithoutDuplicates;
+  }
+
+  static sortTheArr(arr) {
+    if (!(Array.isArray(arr))) throw new Error("The argument is not an array.");
+    if (arr.length === 0) return [];
+    if (arr.length === 1) return arr;
+    if (arr.length > 1) {
+      const middleOfArr = Math.floor(arr.length / 2);
+    
+      const leftHalfOfArr = arr.slice(0, middleOfArr);
+      const rightHalfOfArr = arr.slice(middleOfArr);
+
+      const mergeSortLeftHalfOfArr = Tree.sortTheArr(leftHalfOfArr);
+      const mergeSortRightHalfOfArr = Tree.sortTheArr(rightHalfOfArr);
+
+      let tempSortResultOfCurrIteration = [];
+      for (let i = 0, j = 0; i < mergeSortLeftHalfOfArr.length &&
+          j < mergeSortRightHalfOfArr.length;) {
+        if (mergeSortLeftHalfOfArr[i] < mergeSortRightHalfOfArr[j]) {
+          tempSortResultOfCurrIteration.push(mergeSortLeftHalfOfArr[i]);
+          i++;
+        } else {
+          tempSortResultOfCurrIteration.push(mergeSortRightHalfOfArr[j]);
+          j++;
+        };
+      
+        if (i === mergeSortLeftHalfOfArr.length) {
+          for (;j < mergeSortRightHalfOfArr.length; j++) {
+            tempSortResultOfCurrIteration.push(mergeSortRightHalfOfArr[j])
+          };
+          break;
+        };
+        if (j === mergeSortRightHalfOfArr.length) {
+          for (;i < mergeSortLeftHalfOfArr.length; i++) {
+            tempSortResultOfCurrIteration.push(mergeSortLeftHalfOfArr[i]);
+          };
+          break;
+        };
+      };
+      return tempSortResultOfCurrIteration;
+    };
+  };
+
+  buildTree(arr) {
+    if (arr.length === 0) return new Node(arr);
+    if (arr.length === 1) return new Node(arr);
+    if (arr.length === 2) return new Node(arr[1], arr[0]);
+    if (arr.length === 3) return new Node(arr[1], arr[0], arr[2]);
+
+    const middleOfArr = Math.floor(arr.length / 2);
+
+    const leftHalfOfTheArr = arr.slice(0, middleOfArr);
+    const rightHalfOfArr = arr.slice(middleOfArr + 1);
+
+    return new Node(arr[middleOfArr], this.buildTree(leftHalfOfTheArr), this.buildTree(rightHalfOfArr));
   };
 };
 
+const myTree = new Tree(arr);
+const treeRoot = (myTree.root);
+console.log(treeRoot);
 
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-
+/*
 const mySet = new Set(arr);
 const arrWithoutDuplicates = Array.from(mySet.values());
 console.log(arrWithoutDuplicates);
@@ -46,11 +105,11 @@ const mergeSort = function(arr) {
       if (mergeSortLeftHalfOfArr[i] < mergeSortRightHalfOfArr[j]) {
         tempSortResultOfCurrIteration.push(mergeSortLeftHalfOfArr[i]);
         i++;
-      };
-      if (mergeSortLeftHalfOfArr[i] > mergeSortRightHalfOfArr[j]) {
+      } else {
         tempSortResultOfCurrIteration.push(mergeSortRightHalfOfArr[j]);
         j++;
       };
+    
       if (i === mergeSortLeftHalfOfArr.length) {
         for (;j < mergeSortRightHalfOfArr.length; j++) {
           tempSortResultOfCurrIteration.push(mergeSortRightHalfOfArr[j])
@@ -71,77 +130,4 @@ const mergeSort = function(arr) {
 const sortedArr = mergeSort(arrWithoutDuplicates);
 console.log(sortedArr);
 
-
-
-
-
-
-
-
-
-
-
-/*
-let resultArray = [];
-const mergeSort = function(arr) {
-  if (!(Array.isArray(arr))) throw new Error("The argument is not an array.");
-  if (arr.length === 0) return [];
-  if (arr.length === 1) return arr;
-  if (arr.length > 1) {
-    let middleOfArray = Math.floor(arr.length / 2);
-
-    const leftHalf = arr.slice(0,middleOfArray);
-    const rightHalf = arr.slice(middleOfArray, arr.length);
-
-    const firstNum = mergeSort(leftHalf);
-    const secondNum = mergeSort(rightHalf);  
-
-    if (resultArray[0] === undefined) {
-      if (firstNum[0] < secondNum[0]) {
-        resultArray.push(firstNum[0]);
-        resultArray.push(secondNum[0]);
-      } else {
-        resultArray.push(secondNum[0]);
-        resultArray.push(firstNum[0]);
-      };
-      return resultArray;
-    };
-  
-    let sortingArray = [];
-    if (resultArray[0] !== undefined) {
-      let i = 0;
-      let j = 0;
-      for (;i < firstNum.length, j < secondNum.length;) {
-        if (firstNum[i] > secondNum[j]) {
-          sortingArray.push(secondNum[j]);
-          j++;
-        } else if (firstNum[i] < secondNum[j]) {
-          sortingArray.push(firstNum[i]);
-          i++;
-        } else if (firstNum[i] === secondNum[j]) {
-          sortingArray.push(firstNum[i]);
-          sortingArray.push(secondNum[j]);
-          i++;
-          j++;
-        };
-        
-        if (i === firstNum.length) {
-          for (let k = j; k < secondNum.length; k++) {
-            sortingArray.push(secondNum[k]);
-          };
-          resultArray = sortingArray;
-          return resultArray;
-        };
-        if (j === secondNum.length) {
-          for (let k = i; k < firstNum.length; k++) {
-            sortingArray.push(firstNum[k]);
-          };
-          resultArray = sortingArray;
-          return resultArray;
-        };
-      };
-    };
-  };
-};
-console.log(mergeSort(arr));
 */
